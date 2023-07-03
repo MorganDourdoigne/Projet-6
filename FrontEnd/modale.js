ab991140af1dce897eb2190290c3036c77a05331
MorganDourdoigne <dourdoigne.morgan@gmail.com>
2023-07-03 15:43:16 +0200

    clic modifier et X ok

---
+++ FrontEnd/modale.js
@@ -0,0 +1,34 @@
+//Fonction pour ouvrir la modale
+let openModale = function (e) {
+    e.preventDefault()
+    const modal = document.querySelector(".js_modale");
+    modal.removeAttribute("aria-hidden")
+    modal.setAttribute("aria-modal","true")
+    modal.style.display = "block"
+}
+// ouverture en cliquant sur une div "modifier"
+const open = document.querySelectorAll(".modify_button");
+open.forEach(open =>{
+       open.addEventListener("click",openModale)       
+})
+
+//fonction pour fermer la modale
+let modal = document.querySelector(".js_modale");
+const closeModal = function (e){
+    if (modal === null) return
+    e.preventDefault()
+    modal.style.display = "none"
+    modal.setAttribute("aria-hidden", "true")
+    modal.removeAttribute("aria-modal")
+    modal.removeEventListener("click",closeModal)
+    
+}
+// fermeture en cliquant sur l'icone X
+const close = document.querySelector(".logoX");
+close.addEventListener("click",closeModal);
+
+// fermeture clique en dehors de la modale
+let back = document.querySelector(".modale1");
+back.addEventListener("click", closeModal);
+
+
