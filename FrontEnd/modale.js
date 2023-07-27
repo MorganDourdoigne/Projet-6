75fc50520845946a7b0784bd07e81103787f72ba
MorganDourdoigne <dourdoigne.morgan@gmail.com>
2023-07-27 10:05:41 +0200

    envoi des données sans rafraischissement

--- FrontEnd/modale.js
+++ FrontEnd/modale.js
@@ -34,11 +34,11 @@ document.getElementById('close_modale2').addEventListener("click", function(e) {
 
 
 // clic en dehors de la modale
-const backgroundModal = document.querySelector("#window1");
-backgroundModal.addEventListener("click", function(myEvent) {
-  myEvent.preventDefault();
-  closeModal();
-});
+// const backgroundModal = document.querySelector("#window1");
+// backgroundModal.addEventListener("click", function(myEvent) {
+//   myEvent.preventDefault();
+//   closeModal();
+// });
 
 
 // flèche pour passer de window2 à window1
@@ -60,42 +60,114 @@ document.getElementById("add_photo1").addEventListener("click", function(){
 
 
 // mise en place de la gallerie dans la modale en javascript
-const galleryMini = document.getElementById("galleryMini");
-fetch("http://localhost:5678/api/works")
+// const galleryMini = document.getElementById("galleryMini");
+// fetch("http://localhost:5678/api/works")
+//   .then(response => response.json())
+//   .then(data => {
+//     data.forEach(work => {
+//       const sizeImg = document.createElement("div");
+//       sizeImg.classList.add("size_img");
+
+//       const container = document.createElement("div");
+//       container.classList.add("container");
+
+//       const img = document.createElement("img");
+//       img.src = work.imageUrl;
+//       img.alt = work.title;
+//       container.appendChild(img);
+
+//       const edit = document.createElement("div");
+//       edit.classList.add("edit");
+//       const editText = document.createTextNode("éditer");
+//       edit.appendChild(editText);
+//       container.appendChild(edit);
+
+//       const trash = document.createElement("i");
+//       trash.classList.add("fa-regular", "fa-trash-can", "icon");
+//       container.appendChild(trash);
+// // fonction lors du clic pour retirer l'élément dans l'api  par ID
+//       trash.addEventListener("click", () => {
+//   deleteWork(work.id, trash);
+//   sizeImg.remove();
+// });
+//      sizeImg.appendChild(container);
+
+//       galleryMini.appendChild(sizeImg);
+//     });
+//   })
+
+  function createGallery (){
+    const galleryMini = document.getElementById("galleryMini");
+    fetch("http://localhost:5678/api/works")
+      .then(response => response.json())
+      .then(data => {
+        data.forEach(work => {
+          const sizeImg = document.createElement("div");
+          sizeImg.classList.add("size_img");
+    
+          const container = document.createElement("div");
+          container.classList.add("container");
+    
+          const img = document.createElement("img");
+          img.src = work.imageUrl;
+          img.alt = work.title;
+          container.appendChild(img);
+    
+          const edit = document.createElement("div");
+          edit.classList.add("edit");
+          const editText = document.createTextNode("éditer");
+          edit.appendChild(editText);
+          container.appendChild(edit);
+    
+          const trash = document.createElement("i");
+          trash.classList.add("fa-regular", "fa-trash-can", "icon");
+          container.appendChild(trash);
+    // fonction lors du clic pour retirer l'élément dans l'api  par ID
+          trash.addEventListener("click", (e) => {
+            e.preventDefault();
+      deleteWork(work.id, trash);
+      sizeImg.remove();
+      const gallery = document.querySelector(".gallery");
+      gallery.innerHTML = "";
+      fetchWorks();
+    });
+         sizeImg.appendChild(container);
+    
+          galleryMini.appendChild(sizeImg);
+        });
+      })
+    
+  }
+
+
+
+
+
+  function fetchWorks (){
+    fetch("http://localhost:5678/api/works")
+// envoi d'une demande pour avoir un retour en format JSon
   .then(response => response.json())
   .then(data => {
-    data.forEach(work => {
-      const sizeImg = document.createElement("div");
-      sizeImg.classList.add("size_img");
-
-      const container = document.createElement("div");
-      container.classList.add("container");
-
+    // sélection de la class gallery qui contient les images
+    const gallery = document.querySelector(".gallery");
+    // boucle "for" "in" qui parcourt chaque élément dans le retour json
+    for (let i in data) {
+      const figure = document.createElement("figure");
+      gallery.appendChild(figure);
+      /*"img" et figcaption", même procédé:
+       -création des variables "img" et "figcaption", 
+       -recherche dans le array de l'api 
+       -rattachement des 2 éléments à la class parent "gallery"*/
       const img = document.createElement("img");
-      img.src = work.imageUrl;
-      img.alt = work.title;
-      container.appendChild(img);
+      img.src = data[i].imageUrl;
+     figure.appendChild(img);
 
-      const edit = document.createElement("div");
-      edit.classList.add("edit");
-      const editText = document.createTextNode("éditer");
-      edit.appendChild(editText);
-      container.appendChild(edit);
-
-      const trash = document.createElement("i");
-      trash.classList.add("fa-regular", "fa-trash-can", "icon");
-      container.appendChild(trash);
-// fonction lors du clic pour retirer l'élément dans l'api  par ID
-      trash.addEventListener("click", () => {
-  deleteWork(work.id, trash);
-  sizeImg.remove();
-});
-
-      sizeImg.appendChild(container);
-
-      galleryMini.appendChild(sizeImg);
-    });
-  })
+      const figcaption = document.createElement("figcaption");
+      figcaption.innerHTML = data[i].title;
+      figure.appendChild(figcaption);
+    }
+  });
+  }
   
   
     // fonction qui annule en faisant un fetch un id 
@@ -148,7 +220,15 @@ function sendAPI() {
     body: formData
    }).then(response => {
     if (response.ok) {
-      alert("L'image a été envoyée avec succès!");
+            alert("L'image a été envoyée avec succès!");
+            const gallery = document.querySelector(".gallery");
+            const galleryMini = document.getElementById("galleryMini");
+            const form = document.getElementById("form");
+            gallery.innerHTML = "";
+            galleryMini.innerHTML = "";
+            fetchWorks();
+            form.reset();
+            createGallery();
     } 
   })
 }
@@ -172,3 +252,6 @@ function changeButton() {
 document.getElementsByName("title")[0].addEventListener("input", changeButton);
 document.getElementById("category").addEventListener("input", changeButton);
 document.querySelector('input[type=file]').addEventListener("input", changeButton);
+
+createGallery();
+console.log(document.getElementById("form"))
